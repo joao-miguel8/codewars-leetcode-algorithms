55c025138feffd4fba9d25e6d46ba6c165f54de6
[joao miguel] <[joaocun81@gmail.com]>
2023-05-19 18:56:33 -0400

    completed another codewars challenge.

---
+++  string-repeat.js
@@ -0,0 +1,21 @@
+/*
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+*/
+
+const repeatStr = (n, s) => s.repeat(n);
+
+/*
+Alternative Way =
+
+function repeatStr(num,string) {
+    let strRepeated = '';
+  for(let i = 0; i < num; i++) {
+    strRepeated += string; 
+  }
+    return strRepeated;
+}
+*/
