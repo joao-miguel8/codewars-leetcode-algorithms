88aedf1e3e04e4ca4d0c000b724437332975b502
[joao miguel] <[joaocun81@gmail.com]>
2023-05-25 20:08:13 -0400

    completed two codewars challenges

---
+++ string-templates-bug-fixing-#5.js
@@ -0,0 +1,11 @@
+
+/*
+Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+function buildString(...template){
+// code here
+}
+*/
+
+
+const buildString = (...template) => `I like ${template.join(", ")}!`;
