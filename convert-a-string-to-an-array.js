b8b60fb0054f33121ea7f007dc2a6ba58a98abc3
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-05-25 21:24:13 -0400

    completed 1 code wars challenge

---
+++ convert-a-string-to-an-array.js
@@ -0,0 +1,15 @@
+/*
+Write a function to split a string and convert it into an array of words.
+
+Examples (Input ==> Output):
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+stringToArray(string) { 
+// code here
+}
+
+*/
+
+const stringToArray = (string) => string.split(" ");
