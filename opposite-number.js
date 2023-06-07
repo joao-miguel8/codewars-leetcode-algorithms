7311609875fcd084cf4b5d812ce5d53966e0357b
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-06-06 23:03:46 -0400

    completed a new code wars challenge

---
+++ opposite-number.js
@@ -0,0 +1,17 @@
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+function oppisite() {
+  // code here
+}
+*/
+
+function opposite(n) {
+  return n * -1
+}
