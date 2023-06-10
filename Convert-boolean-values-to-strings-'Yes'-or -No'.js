8637a88af239c9ec7f970aae13b0cdfac961e151
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-06-09 23:12:57 -0400

    completed another 8kyu codewars challenge

---
+++ Convert-boolean-values-to-strings-'Yes'-or -No'.js
@@ -0,0 +1,10 @@
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord (bool) {
+  //code here
+} 
+*/
+
+const boolToWord = (bool) => bool === true ? "Yes" : "No";
+
