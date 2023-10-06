902602ef3ab54dd3cab1ffc9fe80861e2693845d
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-10-06 19:01:38 -0400

    evert "add leetcode directory add codewars directory and completed a leet code challenge"
    This reverts commit 3d2e43aa157970999e5c3ff6048bcab6dfaf6e07.

---  Scrabblemania.js
+++  Scrabblemania.js
@@ -22,53 +22,35 @@ const letterPointsSystem = {
 
 */
 
-function wordScore(word) {
-	const letterPointsSystem = {
-		a: 1,
-		b: 3,
-		c: 3,
-		d: 2,
-		e: 1,
-		f: 4,
-		g: 2,
-		h: 4,
-		i: 1,
-		j: 8,
-		k: 5,
-		l: 1,
-		m: 3,
-		n: 1,
-		o: 1,
-		p: 3,
-		q: 10,
-		r: 1,
-		s: 1,
-		t: 1,
-		u: 1,
-		v: 4,
-		w: 4,
-		x: 8,
-		y: 4,
-		z: 10,
-	};
+function wordscore(word) {
+  
+  const letterPointsSystem = {
+  "a" : 1, "b" : 3,"c" : 3,"d" : 2,
+  "e" : 1, "f" : 4,"g" : 2,"h" : 4,
+  "i" : 1,"j" : 8,"k" : 5,"l" : 1,
+  "m" : 3,"n" : 1,"o" : 1,"p" : 3,
+  "q" : 10,"r" : 1,"s" : 1,"t" : 1,
+  "u" : 1,"v" : 4, "w" : 4,"x" : 8,
+  "y" : 4,"z" : 10
+  }
 
-	const wordIntoLetters = word.split("");
-	let sumOfLetters = 0;
-	for (let i = 0; i < wordIntoLetters.length; i++) {
-		sumOfLetters += letterPointsSystem[wordIntoLetters[i]];
-	}
-
-	let result = sumOfLetters * wordIntoLetters.length;
-
-	if (wordIntoLetters.length === 7) {
-		return (result += 50);
-	}
-	return result;
+  const wordIntoLetters = word.split("");
+  
+  let sumOfLetters = 0;
+  
+  for(let i = 0; i < wordIntoLetters.length; i++) {
+    sumOfLetters += letterPointsSystem[wordIntoLetters[i]];
+ }
+  
+  let result =  sumOfLetters * wordIntoLetters.length;
+  
+  if (wordIntoLetters.length === 7) { return result += 50} 
+  return result;
 }
 // PSEUDOCODE:
-//  user has to create a word from the letters in their hand,
+//  user has to create a word from the letters in their hand, 
 // create a function which returns the correct score for the word played by the user?
-
+ 
 // The score of the word is the sum of the points of all the letters;
 // multiplied by the number of letters in the word.
 
