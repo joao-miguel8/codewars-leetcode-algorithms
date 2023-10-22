7f1ccced58a5ee810b134e449ee7a1a03909cd2a
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-10-22 11:05:31 -0400

    completed a new string algorithm challenge

---
+++ string-challanges/what's-hiding-amongst-the-crowd.js
@@ -0,0 +1,23 @@
+/* 
+What's Hiding Amongst the Crowd?
+A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
+
+The wanted word is in lowercase.
+The crowd of letters is all in uppercase.
+Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
+Examples
+detectWord("UcUNFYGaFYFYGtNUH") ➞ "cat"
+detectWord("bEEFGBuFBRrHgUHlNFYaYr") ➞ "burglar"
+detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment") ➞ "embezzlement"
+*/
+
+const detectWord = str =>
+	str.split("").reduce((acc, currentVal) => {
+		if (currentVal === currentVal.toLowerCase()) {
+			acc += currentVal;
+		}
+		return acc;
+	}, "");
+
+console.log(detectWord("GLwTAeFAWlPQcKMAoAmRAWe"));
+console.log(detectWord("RWQPcWQEQMoWWQdJKeLAOWr"));
